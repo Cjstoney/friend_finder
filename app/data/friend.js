1e4f291072a8b2ec75ea3946efbabf206cb1fffb
Camerone Stoney <Cameronestoney@yahoo.com>
2019-07-11 12:44:13 -0700

    friend database created

---
+++ app/data/friend.js
@@ -0,0 +1,26 @@
+var options = [
+    {
+        name: "Bob Vance",
+        scores:[5,3,5,3,1,3,2,5,4,1]
+    },
+    {
+        name: "Ryan The Temp",
+        scores:[5,4,5,0,1,3,2,3,4,4]
+    },
+    {
+        name: "Ryan The Exec",
+        scores:[5,5,5,5,5,5,5,5,5,0]
+    },
+    {
+        name: "Oscar Martinez",
+        scores:[2,3,0,5,1,3,1,5,0,1]
+    },
+    {
+        name: "Creed Bratton",
+        scores:[1,0,0,0,0,3,2,0,4,1]
+    },
+    {
+        name: "Dwight Schrute",
+        scores:[3,3,3,3,3,3,3,3,3,3]
+    }
+]
